e56f9571d412e44bb1b230c3be3ae4a0e7c34308
bao <you@example.com>
2019-06-25 14:10:50 +0800

    tesat

--- jarden_center/main_code/single-multiple-source/baseon_setCover/test/Some_Example/randomTest.py
+++ jarden_center/main_code/single-multiple-source/baseon_setCover/test/Some_Example/randomTest.py
@@ -8,7 +8,7 @@ import xlrd
 
 from datetime import date,datetime
 
-file = 'test3.xlsx'
+file = '数据.xlsx'
 
 def read_excel():
 
@@ -18,41 +18,48 @@ def read_excel():
 
 	sheet1 = wb.sheet_by_index(0)#通过索引获取表格
 
-	sheet2 = wb.sheet_by_name('年级')#通过名字获取表格
-
-	print(sheet1,sheet2)
-
+	print(sheet1)
 	print(sheet1.name,sheet1.nrows,sheet1.ncols)
+	rowfangcha=[]
+	for  i  in range(sheet1.nrows):
+		rows=sheet1.row_values(i)
+		arr_var = np.var(rows)
+		rowfangcha.append(arr_var)
+	print ('每一行的方差'+str(rowfangcha))
 
-	rows = sheet1.row_values(2)#获取行内容
-
-	cols = sheet1.col_values(3)#获取列内容
-
-	print(rows)
-
-	print(cols)
-
-	print(sheet1.cell(1,0).value)#获取表格里的内容，三种方式
-
-	print(sheet1.cell_value(1,0))
-
-	print(sheet1.row(1)[0].value)
-
-
-
-
-
-
+	rowfangcha.clear()
+	for  j  in range(sheet1.ncols):
+		cols=sheet1.col_values(j)
+		arr_var = np.var(cols)
+		rowfangcha.append(arr_var)
+	print ('每一列的方差'+str(rowfangcha))
+	# rows = sheet1.row_values(2)#获取行内容
+	# cols = sheet1.col_values(3)#获取列内容
+	# print(rows)
+	#
+	# print(cols)
+	#
+	# print(sheet1.cell(1,0).value)#获取表格里的内容，三种方式
+	#
+	# print(sheet1.cell_value(1,0))
+	#
+	# print(sheet1.row(1)[0].value)
 
 
 
-arr = [1,2,3,4,5,6]
-#求均值
-arr_mean = np.mean(arr)
-#求方差
-arr_var = np.var(arr)
-#求标准差
-arr_std = np.std(arr,ddof=1)
-print("平均值为：%f" % arr_mean)
-print("方差为：%f" % arr_var)
-print("标准差为:%f" % arr_std)
+
+
+
+
+read_excel()
+
+# arr = [1,2,3,4,5,6]
+# #求均值
+# arr_mean = np.mean(arr)
+# #求方差
+# arr_var = np.var(arr)
+# #求标准差
+# arr_std = np.std(arr,ddof=1)
+# print("平均值为：%f" % arr_mean)
+# print("方差为：%f" % arr_var)
+# print("标准差为:%f" % arr_std)
