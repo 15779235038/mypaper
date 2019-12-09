0fd9fc4ef1c2b771b7517e6d05962ca13f157733
baozhiqiang <530420396@qq.com>
2019-12-09 10:20:08 +0800

    rumor ceneter

---
+++ jarden_center/research-master/research-master/SourceDetection/bao/map_gsba_bao3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File  : map_gsba_bao3.py
+# @Author: zhiqiangbao
+# @Date  : 2019/12/9
+
+'''
+
+贪心构建的序列中，不应该只是单纯考虑该节点和已经感染节点的权重和连边，
+或许也可以考虑该点覆盖率来加它。
+
+
+
+
+
+'''
